20ede35f200f26c8cd3e8d57cd3bb9a7f0bc0033
HosseinrezaAk <hrakbari77@gmail.com>
2024-08-15 17:47:37 +0330

    add mount

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,10 +1,12 @@
 import AppHeader from '@/components/AppHeader.vue'
-
+import { mount } from '@vue/test-utils'
 describe('AppHeader', () => {
   test('If user is not logged in, do not show logout button', () => {
-    expect(true).toBe(true)
+    const wrapper = mount(AppHeader)
+    expect(wrapper.find('button').isVisible()).toBe(false)
   })
   test('If user is logged in, show logout button', () => {
-    expect(true).toBe(true)
+    const wrapper = mount(AppHeader)
+    expect(wrapper.find('button').isVisible()).toBe(true)
   })
 })
