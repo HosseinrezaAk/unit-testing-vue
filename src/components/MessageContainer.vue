08528f969335c3ef163b9efd9f69100443fa9ded
HosseinrezaAk <hrakbari77@gmail.com>
2024-08-19 00:32:16 +0330

    add stub version

---
+++ src/components/MessageContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <MessageDisplay></MessageDisplay>
+  </div>
+</template>
+
+<script>
+import { getMessage } from '@/services/axios'
+import MessageDisplay from './MessageDisplay.vue'
+export default {
+  components: {
+    MessageDisplay,
+  },
+  data() {
+    return {
+      message: {},
+      error: null,
+    }
+  },
+  async created() {
+    try {
+      this.message = await getMessage()
+    } catch (err) {
+      this.error = 'Oops! Something went wrong.'
+    }
+  },
+}
+</script>
+
+<style></style>
