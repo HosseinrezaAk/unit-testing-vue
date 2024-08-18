5e3a412f22c5db3d2ff754b89c235ca115a48dc4
HosseinrezaAk <hrakbari77@gmail.com>
2024-08-19 00:35:42 +0330

    fix issues

--- src/components/MessageContainer.vue
+++ src/components/MessageContainer.vue
@@ -5,25 +5,11 @@
 </template>
 
 <script>
-import { getMessage } from '@/services/axios'
 import MessageDisplay from './MessageDisplay.vue'
 export default {
   components: {
     MessageDisplay,
-  },
-  data() {
-    return {
-      message: {},
-      error: null,
-    }
-  },
-  async created() {
-    try {
-      this.message = await getMessage()
-    } catch (err) {
-      this.error = 'Oops! Something went wrong.'
-    }
-  },
+  }
 }
 </script>
 
